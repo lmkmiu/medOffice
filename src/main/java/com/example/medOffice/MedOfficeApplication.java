b9d3af487b845bcc3b137d7f15860c14237fd44a
miu kuen lee <lmk.miu@gmail.com>
2023-02-22 20:03:31 -0500

    spring initializr

---
+++ src/main/java/com/example/medOffice/MedOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.medOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medOffice/MedOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
