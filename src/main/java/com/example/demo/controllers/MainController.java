ac857e872367c0e670005adb6833c625443ceefc
miu kuen lee <lmk.miu@gmail.com>
2023-03-02 15:11:40 -0500

    sign in

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -3,13 +3,23 @@ package com.example.demo.controllers;
 //import ch.qos.logback.core.model.Model;
 import com.example.demo.entities.*;
 import com.example.demo.repositories.*;
+import jakarta.validation.Valid;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+
+
+import javax.management.remote.JMXAuthenticator;
+import java.io.IOException;
+import java.util.List;
 
 
 @Controller
@@ -32,53 +42,103 @@ public class MainController {
     public String index(Model model) {
         return "index";
     }
-    @PostMapping("/")
-    public String addUser(@ModelAttribute User user) {
+//    @PostMapping("/signup")
+//    public String addUser(Model model, @Valid @ModelAttribute User newUser, BindingResult res) {
+//        if (newUser.getPasswords().trim().equals("")){
+//            FieldError error = new FieldError("user", "password",
+//                    "Password cannot be empty");
+//            res.addError(error);
+//        }
+//        if (res.hasErrors()){
+//            model.addAttribute("user", newUser);
+//
+//            return "/signup";
+//        }
+//        users.save(newUser);
+//        return "redirect:/appointment";
+//    }
+
+    @PostMapping("/signup")
+    public String addUser(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPasswords());
+        user.setPasswords(encodedPassword);
+
         users.save(user);
-        return "redirect:/";
+
+        return "redirect:/appointment";
     }
-//    @GetMapping("/appointment")
-//    public String appointment(Integer id, Model model) {
-//        User user = users.findById(id).orElse(null);
-//        List<Appointment>   userApps = appointments.findByUser(id);
+//    @PostMapping("/")
+//    public String userLogIn(@ModelAttribute User user) {
 //
-//        model.addAttribute("appointments", userApps);
-//
-//        return "redirect:/appointment?user=";
+//        return "redirect:/appointment";
 //    }
-//    @PostMapping("/appointment")
-//    public String makeApp(@ModelAttribute Appointment app) {
-//        appointments.save(app);
-//
-//        return "redirect:/appointment?user=";
-//    }
-//
-//
-//
-//    @GetMapping("/edit")
-//    public String editUser(Integer id, Model model) {
-//        User user = users.findById(id).orElse(null);
-//        model.addAttribute("user", user);
-//        return "/edit";
-//    }
-//
-//    @PostMapping("/editUser")
-//    public String editUser(@ModelAttribute User user, Integer id) {
-//        User updateuser = users.findById(id).orElse(null);
-//
-//        updateuser.setFirstName(user.getFirstName());
-//        updateuser.setLastName(user.getLastName());
-//        updateuser.setEmail(user.getEmail());
-//        updateuser.setPhone(user.getPhone());
-//        updateuser.setNotification(user.isNotification(0));
-//
-//        users.save(updateuser);
-//        return "redirect:/editUser";
-//    }
-//
+
+    @PostMapping("/")
+    public String adminLogIn(@ModelAttribute User user) {
+
+        return "redirect:/schedule";
+    }
+
+    @GetMapping("/editUser/{id}")
+    public String editUser(Model model,  @PathVariable int id) {
+        User user = users.findById(id).orElse(null);
+        model.addAttribute("user", user);
+        return "/editUser";
+    }
+
+    @PostMapping("/editUser")
+    public String editUser(Model model, @Valid @ModelAttribute User updateuser, BindingResult res) {
+        if (updateuser.getPasswords().trim().equals("")){
+            var previousPassword = users.findById(updateuser.getUserId()).get().getPasswords();
+            updateuser.setPasswords(previousPassword);
+        }
+        if (res.hasErrors()){
+            model.addAttribute("user", updateuser);
+            return "editUser";
+        }
+        users.save(updateuser);
+        return "redirect:/editUser";
+    }
+
 //    @GetMapping("/appointment")
 //    public String appointment(Model model) {
+////        User user = users.findById(id).orElse(null);
+//        List<Appointment> appList = appointments.findByUser(user.getUserId());
+//        List<Physician> phyList = physicians.findAll();
+//        model.addAttribute("phyList", phyList);
+//        model.addAttribute("appList", appList);
 //        return "/appointment";
 //    }
+//
+//    @GetMapping("/new_appointment/{id}")
+//    public String newAppointment(Model model,  @PathVariable int id) {
+//        List<Appointment> drApps = appointments.findByPhysician(id);
+//
+//        model.addAttribute("phyList", phyList);
+//        model.addAttribute("appList", appList);
+//        return "/appointment/{id}";
+//    }
+
+    @PostMapping("/new_appointment/{id}")
+    public String addAppointment(Model model,  @PathVariable int id, @Valid @ModelAttribute("appointment") Appointment newApp,
+                                 BindingResult res) throws IOException {
+        List<Appointment> drApps = appointments.findByPhysician(id);
+
+        drApps.forEach(dr -> {
+            if(dr.getStartTime() == newApp.getStartTime()) {
+                FieldError error = new FieldError("Start Time", "Start Time", "Time slot is not available.");
+                res.addError(error);
+            }
+        });
+
+        if (res.hasErrors()) {
+            model.addAttribute("Appointment", newApp);
+            return "/appointment";
+        }
+        appointments.save(newApp);
+
+        return "/appointment";
+    }
     
 }
--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.repositories;
 
         import com.example.demo.entities.User;
         import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.jpa.repository.Query;
         import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+        @Query("SELECT u FROM User u WHERE u.email = ?1")
+        public User findByEmail(String email);
 }
